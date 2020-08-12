65a150bcde48a31b332fee8bd61d36caeae849fe
drcosquared <69602421+drcosquared@users.noreply.github.com>
2020-08-12 15:34:16 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='wes',
-      version='0.1.5post2',
+      version='0.17',
       description='Use Wes Anderson Color Palettes in Matplotlib & Seaborn',
       url='https://github.com/ljwolf/wampl',
       author='Levi John Wolf',
@@ -9,6 +9,5 @@ setup(name='wes',
       license='CC-BY-SA',
       packages=['wes'],
       install_requires=['matplotlib'],
-      package_data ={'':'colors.json'},
       include_package_data=True,
       zip_safe=False)
